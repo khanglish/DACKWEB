cfb20d4c5a2a90862046b99ea1616e20ab04f4e2
vuthuakhangit <vuthuakhangit@gmail.com>
2018-05-08 23:14:33 +0700

    fix price display javascript

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -12,11 +12,11 @@ function covert(string) {
     string = string.trim();
     let arr = [];
     let i = string.length -1;
-    while (i - 3 > 0) {
-        arr.push(string.substr(i-2, i));
+    while (i - 3 > -1) {
+        arr.push(string.substring(i-3, i));
         i = i -3;
     }
-    arr.push(string.substr(0, i+1));
+    arr.push(string.substring(0, i+1));
     let result = "";
     while (arr.length > 1) {
         result += arr.pop() + '.';
