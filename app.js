a084f99ba26b2aa37b46b373a6787ee658b93933
KHANG <khangvuong97@gmail.com>
2018-05-14 21:51:39 +0700

    Success-Details-Page

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var expressHsb = require('express-handlebars');
 var mongoose = require('mongoose');
-
 var indexRouter = require('./routes/index');
+
 // var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,10 +21,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
-// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var Laptop = require('../models/laptops');
 var Pc = require('../models/pcs');
 var Monitor = require('../models/monitors');
 /* GET home page. */
-
 router.get('/', function(req, res, next) {
   Laptop.find().exec(function(err, laptops) {
     Pc.find().exec(function(err, pcs) {
@@ -33,4 +32,22 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/laptops/:model', function(req, res) {
+  Laptop.findOne({'model': req.params.model}, function(err, lap) {
+    if (err) return;
+    res.render('shop/lap', {lap: lap});
+  });
+});
+router.get('/pcs/:model', function(req, res) {
+  Pc.findOne({'model': req.params.model}, function(err, pc) {
+    if (err) return;
+    res.render('shop/pcs', {pc: pc,});
+  });
+});
+router.get('/monitors/:model', function(req, res) {
+  Monitor.findOne({'model': req.params.model}, function(err, monitor) {
+    if (err) return;
+    res.render('shop/monitor', {monitor:monitor});
+  });
+});
 module.exports = router;
