60568f0304cc26de2d153fbb33f6146dd8a343b8
GOBTH <dangduykhang97@gmail.com>
2018-06-22 22:45:07 +0700

    Chon su dung passport, trang login don gian

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var bodyParser=require('body-parser');
+var Passport=require('passport');
+var LocalStrategy=require('passport-local').Strategy;
 var router = express.Router();
 
 var indexController = require('../controllers/index_controllers');
@@ -11,5 +14,19 @@ router.get('/', indexController.display_product);
 router.get('/:type/details/:model', detailsControllers.display_details);
 
 router.get('/:type/phanloai/:label', caterogyControllers.caterogy);
+
+router.use(bodyParser.urlencoded({extended: true}));
+router.use(Passport.initialize());
+router.use(Passport.session());
+
+router.route('/login')
+.get((req,res)=>res.render('../views/log in/login'));
+.post(Passport.authenticate('local',{failureRedirect: '/login'}))
+
+Passport.use(new LocalStrategy(
+	(username,password,done)=>{
+		
+	}
+))
 
 module.exports = router;
