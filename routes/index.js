3d86ed7ce5b0630b814ed7049a519e255d2c6a7d
GOBTH <dangduykhang97@gmail.com>
2018-06-29 17:02:58 +0700

    add mongo

--- routes/index.js
+++ routes/index.js
@@ -37,6 +37,25 @@ router.get('/add-to-cart/:id', function(req,res,next){
 	});
 });
 
+router.get('/reduce/:id', function(req,res,next){
+	var productId=req.params.id;
+	var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+	cart.reduceByOne(productId);
+	req.session.cart=cart;
+	res.redirect('/shopping-cart');
+});
+
+
+router.get('/remove/:id', function(req,res,next){
+	var productId=req.params.id;
+	var cart = new Cart(req.session.cart ? req.session.cart : {});
+
+	cart.removeItem(productId);
+	req.session.cart=cart;
+	res.redirect('/shopping-cart');
+});
+
 router.get('/add-to-cartt/:id', function(req,res,next){
 	var productId = req.params.id;
 	var cart = new Cart(req.session.cart ? req.session.cart : {});
